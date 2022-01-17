9bd7ac32c2cfadca45437af2e5785697bdaf2ad1
phong1-1 <peterhongnike@gmail.com>
2022-01-17 14:05:46 -0800

    Finalized AsciiBox Project

--- Unit00/src/AsciiBox.java
+++ Unit00/src/AsciiBox.java
@@ -10,8 +10,37 @@ public class AsciiBox
 {
 	public static void main(String[] args)
 	{
-		System.out.println("name \t  date \n\n" );
-		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("Peter Hong \t  1/17/2022 \n\n" );
+
+
+
+	      System.out.println("                +++++++++++++++++++++++++                " );
+	      System.out.println("                +++++++++++++++++++++++++                " );
+	      System.out.println("                +++++++++++++++++++++++++                " );
+	      System.out.println("                AAAAAAAAAAAAAAAAAAAAAAAAA                " );
+	      System.out.println("                AAAAAAAAAAAAAAAAAAAAAAAAA                " );
+	      System.out.println("                +++++++++++++++++++++++++                " );
+	      System.out.println("                +++++++++++++++++++++++++                " );
+	      System.out.println("                +++++++++++++++++++++++++                " );
+	      System.out.println("                AAAAAAAAAAAAAAAAAAAAAAAAA                " );
+	      System.out.println("                AAAAAAAAAAAAAAAAAAAAAAAAA                " );
+	      System.out.println("                +++++++++++++++++++++++++                " );
+	      System.out.println("                +++++++++++++++++++++++++                " );
+	      System.out.println("                +++++++++++++++++++++++++                " );
+	      System.out.println("                AAAAAAAAAAAAAAAAAAAAAAAAA                " );
+	      System.out.println("                AAAAAAAAAAAAAAAAAAAAAAAAA                " );
+	      System.out.println("                +++++++++++++++++++++++++                " );
+	      System.out.println("                +++++++++++++++++++++++++                " );
+	      System.out.println("                +++++++++++++++++++++++++                " );
 
 	}
 }
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
