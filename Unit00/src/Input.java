ebb6c0f81bc7dd0974ed618f41ea9f58f78034ad
phong1-1 <peterhongnike@gmail.com>
2022-01-17 14:23:42 -0800

    Finalized Input.java Lab Assignment
    This is the updated code for my Input.java Lab Assignment

--- Unit00/src/Input.java
+++ Unit00/src/Input.java
@@ -24,9 +24,26 @@ public class Input
 		System.out.print("Enter an integer :: ");
 		intOne = keyboard.nextInt();
 
-
 		System.out.print("Enter an integer :: ");
 		intTwo = keyboard.nextInt();
+		
+		System.out.print("Enter a double :: ");
+		doubleOne = keyboard.nextDouble();
+
+		System.out.print("Enter a double :: ");
+		doubleTwo = keyboard.nextDouble();
+		
+		System.out.print("Enter a float :: ");
+		floatOne = keyboard.nextFloat();
+
+		System.out.print("Enter a float :: ");
+		floatTwo = keyboard.nextFloat();
+		
+		System.out.print("Enter a short :: ");
+		shortOne = keyboard.nextShort();
+
+		System.out.print("Enter a short :: ");
+		shortTwo = keyboard.nextShort();
 
 
 		//add in input for all variables
@@ -35,6 +52,12 @@ public class Input
 		System.out.println();
 		System.out.println("integer one = " + intOne );
 		System.out.println("integer two = " + intTwo );
+		System.out.println("double one = " + doubleOne );
+		System.out.println("double two = " + doubleTwo );
+		System.out.println("float one = " + floatOne );
+		System.out.println("float two = " + floatTwo );
+		System.out.println("short one = " + shortOne );
+		System.out.println("short two = " + shortTwo );
 
 		//add in output for all variables
 
