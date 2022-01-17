1e9e74f548b0502cd87c2835b839d98ec9a7ae2f
phong1-1 <peterhongnike@gmail.com>
2022-01-17 14:15:29 -0800

    Finalized my AsciiArt Lab Assignment
    This is my creature I drew for the AsciiArt Lab Asssignment

--- Unit00/src/AsciiArt.java
+++ Unit00/src/AsciiArt.java
@@ -8,29 +8,25 @@
 
 public class AsciiArt
 {
-    public static void main ( String[] args )
-    {
-        System.out.println("Peter Hong" );
-        System.out.println("Date: 1/17/2022" );
+	public static void main ( String[] args )
+	{
+		System.out.println("Peter Hong 1/17/2022 \n\n" );
+		System.out.println("This is my AsciiArt Lab Creature Below: " );
+		System.out.println("\n\n\n\n" );
 
-
-        System.out.println("                +++++++++++++++++++++++++                " );
-        System.out.println("                +++++++++++++++++++++++++                " );
-        System.out.println("                +++++++++++++++++++++++++                " );
-        System.out.println("                AAAAAAAAAAAAAAAAAAAAAAAAA                " );
-        System.out.println("                AAAAAAAAAAAAAAAAAAAAAAAAA                " );
-        System.out.println("                +++++++++++++++++++++++++                " );
-        System.out.println("                +++++++++++++++++++++++++                " );
-        System.out.println("                +++++++++++++++++++++++++                " );
-        System.out.println("                AAAAAAAAAAAAAAAAAAAAAAAAA                " );
-        System.out.println("                AAAAAAAAAAAAAAAAAAAAAAAAA                " );
-        System.out.println("                +++++++++++++++++++++++++                " );
-        System.out.println("                +++++++++++++++++++++++++                " );
-        System.out.println("                +++++++++++++++++++++++++                " );
-        System.out.println("                AAAAAAAAAAAAAAAAAAAAAAAAA                " );
-        System.out.println("                AAAAAAAAAAAAAAAAAAAAAAAAA                " );
-        System.out.println("                +++++++++++++++++++++++++                " );
-        System.out.println("                +++++++++++++++++++++++++                " );
-        System.out.println("                +++++++++++++++++++++++++                " );
-    }
+		System.out.println("            ___________     " );
+		System.out.println("           |           |    " );
+		System.out.println("           |           |    " );
+		System.out.println("           |  0    0   |    " );
+		System.out.println("           |     <     |    " );
+		System.out.println("           |___________|    " );
+		System.out.println("                 |          " );
+		System.out.println("                 |          " );
+		System.out.println("                 |          " );
+		System.out.println("          -------|-------   " );
+		System.out.println("                 |          " );
+		System.out.println("                 |          " );
+		System.out.println("                / \\        " );
+		
+	}
 }
