f430ddcaf467320c7ab725301d37342fc06d5876
phong1-1 <peterhongnike@gmail.com>
2022-01-17 17:32:55 -0800

    Finalized Variables.java Lab
    I updated and finished my work for the Variables.java Lab

--- Unit00/src/Variables.java
+++ Unit00/src/Variables.java
@@ -18,25 +18,46 @@ public class Variables
 
 		//integer variables
 		byte byteOne = 127;
+		short shortOne = 768;
+		int intOne = 324234;
+		long longOne = 3333333;
 
 
 		//decimal variables
+		float floatOne = 65.21313f;
+		double doubleOne = 234.232323;
 
 
 		//other integer types
+		char charOne = 'G';
 
 
 		//other types
+		boolean booleanOne = true;
+		String stringOne = "hello world";
 
 
 		//output your information here
 		System.out.println("/////////////////////////////////");
+		System.out.println("Peter Hong              1/17/2022");
 		System.out.println("*                               *");
 		System.out.println("*        integer types          *");
 		System.out.println("*                               *");
 		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
-
-
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*        real types             *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - floatOne = "+floatOne+"\t*");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"*");
+		System.out.println("*                               *");
+		System.out.println("*        other integer types    *");
+		System.out.println("*                               *");
+		System.out.println("*booleanOne = "+booleanOne+"\t\t*");
+		System.out.println("*stringOne = "+stringOne+"\t*");
+		
 
 
 
