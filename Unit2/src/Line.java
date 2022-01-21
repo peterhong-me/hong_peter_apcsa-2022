dd6d9fb8364d470ab93bf3f47288e9a20d888297
phong1-1 <peterhongnike@gmail.com>
2022-01-20 19:58:17 -0800

    Final Changes to Unit2 Lab

--- Unit2/src/Line.java
+++ Unit2/src/Line.java
@@ -7,7 +7,7 @@ public class Line
 {
 	public static double getSlope( int x1, int y1, int x2, int y2 )
 	{
-		return 0;
+		return (double) (y2 - y1)/(x2 - x1);
 	}
 
 }
--- Unit2/src/Trap.java
+++ Unit2/src/Trap.java
@@ -8,6 +8,6 @@ public class Trap
 {
 	public static double area( int base1, int base2, int height )
 	{
-		return 0;
+		return (double) ((base1 + base2) * height * 1/2);
 	}
 }
