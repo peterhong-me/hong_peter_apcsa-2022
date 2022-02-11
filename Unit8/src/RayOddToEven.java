7866a3fecfe03b799763a1137c691a2e6785130e
phong1-1 <peterhongnike@gmail.com>
2022-02-10 19:47:19 -0800

    Finalized RaySum and RayOddToEven

--- Unit8/src/RayOddToEven.java
+++ Unit8/src/RayOddToEven.java
@@ -5,8 +5,40 @@
 
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	private int[] ray;
+	
+	public RayOddToEven()
 	{
-		return false;
+		setRay(new int[]{0, 0});
+	}
+	
+	public RayOddToEven(int[] inputRay)
+	{
+		setRay(inputRay);
+	}
+	
+	public void setRay(int[] inputRay)
+	{
+		ray = inputRay;
+	}
+	
+	public int go(int[] ray)
+	{
+		int oddIndex;
+		int evenIndex;
+		for (int i = 0; i < ray.length; i++)
+		{
+			if (ray[i] % 2 != 0) {
+				oddIndex = i;
+				for (int odd = i; odd < ray.length; odd++)
+				{
+					if (ray[odd] % 2 == 0) {
+						evenIndex = odd;
+						return evenIndex-oddIndex;
+					}
+				}
+			}
+		}
+		return -1;
 	}
 }
--- Unit8/src/RaySumLast.java
+++ Unit8/src/RaySumLast.java
@@ -7,6 +7,20 @@ public class RaySumLast
 {
 	public static int go(int[] ray)
 	{
-		return 0;
-	}
+	
+		int last = ray[ray.length-1];
+		int sum= 0;
+		
+		for (int i = 0; i < ray.length-1; i++) {
+			if (ray[i] > last) {
+				sum = sum + ray[i];
+			}
+		}
+		
+		if (sum == 0) {
+			sum = -1;
+		}
+		
+		return sum;
+	}	
 }
